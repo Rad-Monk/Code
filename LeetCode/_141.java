03774dd6ebfa07401a4bc61a757e6e77d5fd921b
Rad_Monk <radmonk8979@gmail.com>
2024-03-06 14:44:09 +0530

    day 55, _141

--- LeetCode/_141.java
+++ LeetCode/_141.java
@@ -1,21 +1,21 @@
-//public class _141 {
-//    public boolean hasCycle(ListNode head) {
-//        if (head == null || head.next == null) {
-//            return false;
-//        }
-//
-//        ListNode slow = head;
-//        ListNode fast = head;
-//
-//        while (fast.next != null && fast.next.next != null) {
-//            fast = fast.next.next;
-//            slow = slow.next;
-//
-//            if (slow == fast) {
-//                return true;
-//            }
-//        }
-//
-//        return false;
-//    }
-//}
+public class _141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
