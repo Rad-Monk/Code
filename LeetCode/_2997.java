9d3589cb6c0048bdfe69b5e89fe9fb09ab116c5a
Rad_Monk <radmonk8979@gmail.com>
2024-04-29 09:12:59 +0530

    day 92, _2997

---
+++ LeetCode/_2997.java
@@ -0,0 +1,17 @@
+class _2997 {
+    public int minOperations(int[] nums, int k) {
+        int ans = 0;
+        for (int x: nums){
+            ans = ans^x;
+        }
+        ans= ans ^k;
+        int res = 0;
+        while(ans > 0){
+            if((ans & 1) !=0 ){
+                res++;
+            }
+            ans = ans >> 1;
+        }
+        return res;
+    }
+}
