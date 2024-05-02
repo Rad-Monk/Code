460a15db6b5fe2c6e12768c7d735688225597c48
Rad_Monk <radmonk8979@gmail.com>
2024-05-02 09:16:43 +0530

    day 93, _217

---
+++ LeetCode/_217.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class _217 {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) return true;
+            else{
+                map.put(num,0);
+            }
+        }
+        return false;
+    }
+}
